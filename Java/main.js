cf88328c5cd1cab48cc0970cd48c826be9927cb8
Chak100 <107071982+Chak100@users.noreply.github.com>
2022-11-07 20:48:40 -0300

    segunda entrega

--- Java/main.js
+++ Java/main.js
@@ -2,7 +2,7 @@ let Presupuesto;
 let cliente=prompt("Ingrese su nombre")
 let mail = prompt("Ingrese por favor su mail");
 
-let SolicitaPresupuesto = prompt("Desea solicitar un presupuesto?: \n1- Si \n2 - No");
+let SolicitaPresupuesto = prompt("Desea ver nuestras opciones de menú?: \n1- Si \n2 - No");
 
 if(SolicitaPresupuesto != 2 && SolicitaPresupuesto != "No") {
     let Menu = prompt(
@@ -37,20 +37,37 @@ if(SolicitaPresupuesto != 2 && SolicitaPresupuesto != "No") {
             alert("Usted ha elegido la opción de Burger Party la misma tiene un valor de $2500");
             let cantidadPersonas3 = parseInt(prompt("Ingresa cantidad de personas"));
             if (cantidadPersonas3 >= 0) {
-                Presupuesto = 4000*cantidadPersonas3;
+                Presupuesto = 2500*cantidadPersonas3;
                 alert("El monto a abonar es de $" + Presupuesto);
             }
             break;
         default:
             alert("Opción no válida");
-            break;
+            
         }
         opcion = prompt(
         "Quiere elegir otra opción?: \n1- Pizza Party. \n2 - Finger Food \n3 - Asado gourmet \n4 - Burger party \nPresioná X para finalizar."
         );
-        break;
+        
     } 
-}else{alert("Gracias por contactarnos")}
+}
+//else{alert("Gracias por contactarnos")}
 
+
+const menu=[ 
+    { nombre: "pizza party", precio: 1500, entradas: 4, principales: 10, postre: 1}, 
+    { nombre: "Finger Food", precio: 3500, entradas: 0, principales: 10, postre: 1},
+    { nombre: "Asado Gourmet", precio: 4000, entradas: 4, principales: 2, postre: 1},
+    { nombre: "Burger party", precio: 2500, entradas: 4, principales: 10, postre: 1},
+    { nombre: "Estilo pernil", precio: 2000, entradas: 1, principales: 2, postre: 1},
+]
+
+
+let presupuesto2= parseInt(prompt("Ingresa cual es tu presupuesto por persona, para poder ayudarte con la mejor opción"))
+;
+const resultado = menu.filter ((menu) =>{
+    return menu.precio <= presupuesto2})
+
+console.log (resultado)
 
 alert("Gracias por confiar en nuestros servicios")
